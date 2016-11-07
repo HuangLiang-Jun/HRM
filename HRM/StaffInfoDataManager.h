1a3aa2fd3508a7a573e990084bdf3df372db23b1
HuangLiang-Jun <bboydais@gmail.com>
2016-11-07 10:13:23 +0800

    add File

---
+++ HRM/StaffInfoDataManager.h
@@ -0,0 +1,13 @@
+//
+//  StaffInfoDataManager.h
+//  HRM
+//
+//  Created by huang on 2016/11/7.
+//  Copyright © 2016年 JimSu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaffInfoDataManager : NSObject
+
+@end
---
+++ HRM/StaffInfoDataManager.m
@@ -0,0 +1,13 @@
+//
+//  StaffInfoDataManager.m
+//  HRM
+//
+//  Created by huang on 2016/11/7.
+//  Copyright © 2016年 JimSu. All rights reserved.
+//
+
+#import "StaffInfoDataManager.h"
+
+@implementation StaffInfoDataManager
+
+@end
