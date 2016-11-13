882f539df45cb04516f1792695e535787da574f1
李家舜 <altaica0113@gmail.com>
2016-11-14 02:53:42 +0800

    合法身分證字號辨識完成

--- HRM/StrValidationFilter.m
+++ HRM/StrValidationFilter.m
@@ -15,17 +15,25 @@
 
 + (BOOL)emailValidationFor:(NSString *)email {
     
-    NSArray *subStr = [email componentsSeparatedByString:@"@"];
-    if (subStr.count == 2) {
-        
-        NSString *regexStr = [NSString stringWithFormat:@"@%@%@-._", LEGAL_ALPHA, LEGAL_NUMBER];
-        NSCharacterSet *charSet = [[NSCharacterSet characterSetWithCharactersInString:regexStr] invertedSet];
-        subStr = [email componentsSeparatedByCharactersInSet:charSet];
-        return (subStr.count == 1);
-        
+    NSArray<NSString *> *subEmailStr = [email componentsSeparatedByString:@"@"];
+    switch (subEmailStr.count) {
+            
+        case 2:
+            
+            if (subEmailStr.firstObject.length !=0 && subEmailStr.lastObject.length != 0) {
+                
+                NSString *regexStr = [NSString stringWithFormat:@"@%@%@-._", LEGAL_ALPHA, LEGAL_NUMBER];
+                NSCharacterSet *regexCharSet = [NSCharacterSet characterSetWithCharactersInString:regexStr];
+                NSCharacterSet *emailCharSet = [NSCharacterSet characterSetWithCharactersInString:email];
+                return [regexCharSet isSupersetOfSet:emailCharSet];
+                
+            }
+            return false;
+            
+        default:
+            return false;
+            
     }
-    return (subStr.count == 2);
-
 }
 
 + (BOOL)passwordValidationFor:(NSString *)pwd {
@@ -33,13 +41,62 @@
     if (pwd.length >= 6) {
         
         NSString *regexStr = [NSString stringWithFormat:@"%@%@", LEGAL_ALPHA, LEGAL_NUMBER];
-        NSCharacterSet *charSet = [[NSCharacterSet characterSetWithCharactersInString:regexStr] invertedSet];
-        NSArray *subStr = [pwd componentsSeparatedByCharactersInSet:charSet];
-        return (subStr.count == 1);
+        NSCharacterSet *regexCharSet = [NSCharacterSet characterSetWithCharactersInString:regexStr];
+        NSCharacterSet *pwdCharSet = [NSCharacterSet characterSetWithCharactersInString:pwd];
+        return [regexCharSet isSupersetOfSet:pwdCharSet];
         
     }
-    return (pwd.length >= 6);
+    return false;
     
+}
+
++ (BOOL)idCardNumValidationFor:(NSString *)idCardNum {
+    
+    switch (idCardNum.length) {
+            
+        case 10: {
+            
+            NSString *letterStr = [idCardNum substringToIndex:1];
+            NSString *regexLetterStr = LEGAL_ALPHA;
+            if ([regexLetterStr containsString:letterStr]) {
+                
+                NSString *numStr = [idCardNum substringFromIndex:1];
+                NSCharacterSet *numCharSet = [NSCharacterSet characterSetWithCharactersInString:numStr];
+                NSString *regexNumStr = LEGAL_NUMBER;
+                NSCharacterSet *regexCharSet = [NSCharacterSet characterSetWithCharactersInString:regexNumStr];
+                if ([regexCharSet isSupersetOfSet:numCharSet]) {
+                    
+                    NSString *genderNum = [numStr substringToIndex:1];
+                    if ([genderNum isEqualToString:@"1"] || [genderNum isEqualToString:@"2"]) {
+                        
+                        NSUInteger num = [numStr integerValue];
+                        NSString *capitalizedLetterStr = [letterStr capitalizedString];
+                        NSDictionary *activeLetters = @{@"A":@10, @"B":@11, @"C":@12, @"D":@13, @"E":@14, @"F":@15, @"G":@16, @"H":@17, @"I":@34, @"J":@18, @"K":@19, @"L":@20, @"M":@21, @"N":@22, @"O":@35, @"P":@23, @"Q":@24, @"R":@25, @"S":@26, @"T":@27, @"U":@28, @"V":@29, @"W":@32, @"X":@30, @"Y":@31, @"Z":@33};
+                        int letterNum = [[activeLetters valueForKey:capitalizedLetterStr] intValue];
+                        letterNum = letterNum/10+letterNum%10*9+(int)(num%10);
+                        for (int i = 1; i < 9; i += 1) {
+                            
+                            num = num/10;
+                            letterNum = letterNum+(int)(num%10)*i;
+                            
+                        }
+                        return (letterNum%10 == 0);
+                        
+                    }
+                    return false;
+                    
+                }
+                return false;
+                
+            }
+            return false;
+            
+        }
+            
+        default:
+            return false;
+            
+    }
 }
 
 @end
