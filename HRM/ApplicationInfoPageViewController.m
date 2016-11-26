a56fbc601d171c26db4951491ceef6d8e92dacef
李家舜 <altaica0113@gmail.com>
2016-11-26 23:09:34 +0800

    no message

--- HRM/ApplicationInfoPageViewController.m
+++ HRM/ApplicationInfoPageViewController.m
@@ -12,47 +12,50 @@
 
 @interface ApplicationInfoPageViewController ()
 
-@property (weak, nonatomic) IBOutlet UITextField *subjectField;
+@property (weak, nonatomic) IBOutlet UITextField *typeField;
 @property (weak, nonatomic) IBOutlet UITextField *fromField;
 @property (weak, nonatomic) IBOutlet UITextField *toField;
-@property (weak, nonatomic) IBOutlet UITextView *contentView;
+@property (weak, nonatomic) IBOutlet UITextField *applyTime;
+@property (weak, nonatomic) IBOutlet UITextView *contentTextView;
 
 @end
 
 @implementation ApplicationInfoPageViewController
 
+#pragma mark - View Lifecycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-}
-
-- (void)loadView {
-    [super loadView];
-    ApplicationListPageTableViewController *applicationListPageTableVC = self.navigationController.viewControllers[0];
-    NSDictionary *applicationInfo = applicationListPageTableVC.selectedApplicationInfo;
-    _subjectField.text = applicationInfo[@"Subject"];
-    _subjectField.userInteractionEnabled = false;
-    _fromField.text = applicationInfo[@"From"];
-    _fromField.userInteractionEnabled = false;
-    _toField.text = applicationInfo[@"To"];
-    _toField.userInteractionEnabled = false;
-    _contentView.text = applicationInfo[@"Content"];
-    _contentView.userInteractionEnabled = false;
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
     
-    [self.navigationController setNavigationBarHidden:false];
+    [_typeField setUserInteractionEnabled:false];
+    [_fromField setUserInteractionEnabled:false];
+    [_toField setUserInteractionEnabled:false];
+    [_applyTime setUserInteractionEnabled:false];
+    [_contentTextView setUserInteractionEnabled:false];
+    
 }
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    NSUInteger index = [self.navigationController.viewControllers indexOfObject:self];
+    ApplicationListPageTableViewController *applicationPTableVC = self.navigationController.viewControllers[index-1];
+    NSDictionary *applicationDict = applicationPTableVC.selectedApplicationDict;
+    NSString *applyDate = [applicationDict allKeys].firstObject;
+    NSDictionary *infoDict = [applicationDict objectForKey:applyDate];
+    _typeField.text = [infoDict objectForKey:@"Type"];
+    _fromField.text = [infoDict objectForKey:@"From"];
+    _toField.text = [infoDict objectForKey:@"To"];
+    _applyTime.text = applyDate;
+    _contentTextView.text = [infoDict objectForKey:@"Content"];
+    
+}
+
 
 - (IBAction)confirmApplicationInfoBtnPressed:(UIButton *)sender {
-    [self.navigationController popToRootViewControllerAnimated:true];
+    
+    [self.navigationController popViewControllerAnimated:true];
+    
 }
-
-
 
 @end
