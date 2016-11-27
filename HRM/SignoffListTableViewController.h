b58db56c81268caaa864ead4c8075688e2ba94ec
李家舜 <altaica0113@gmail.com>
2016-11-28 01:49:28 +0800

    主管頁面預備動作

---
+++ HRM/SignoffListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignoffListTableViewController.h
+//  HRM
+//
+//  Created by 李家舜 on 2016/11/28.
+//  Copyright © 2016年 JimSu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignoffListTableViewController : UITableViewController
+
+@end
